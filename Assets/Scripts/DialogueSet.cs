118cc9b0f32ce2dfd629a113a80c3098e0e2cfcb
DubiousDuck <oscar517730@gmail.com>
2024-01-05 23:16:19 +0800

    WIP multiple dialogue sets

---
+++ Assets/Scripts/DialogueSet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSet : MonoBehaviour
+{
+    [SerializeField] private int nSet = 2;
+    [SerializeField] public int whichSet;
+    public Dialogue[] dialogueSet;
+
+}
--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-  public Dialogue dialogue;
+  private Dialogue dialogue;
+  [SerializeField] private DialogueSet currentSet;
 
   public void OnMouseDown ()
   {
+    dialogue = currentSet.dialogueSet[currentSet.whichSet];
     FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
   }
 }
